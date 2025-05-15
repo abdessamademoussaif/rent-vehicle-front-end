19a413aa6d59450dd150ede693bd166a9a160507
abdessamademoussaif <161370224+abdessamademoussaif@users.noreply.github.com>
2025-05-15 04:03:53 +0100

    delete clerk Publishable Key

--- src/main.jsx
+++ src/main.jsx
@@ -58,11 +58,6 @@ const router = createBrowserRouter([
     element:<AboutUs/>
   }
 ])
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
